276923ff96f9f6ee57bedf07d9a08d4ede1f9407
Student <student@achiever69.local>
2016-09-16 11:27:59 +0530

    adding

---
+++ SwiftDemoForrep/SwiftDemoForrep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwiftDemoForrep
+//
+//  Created by Student on 16/09/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwiftDemoForrep/SwiftDemoForrep/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  SwiftDemoForrep
+//
+//  Created by Student on 16/09/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"hello");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
